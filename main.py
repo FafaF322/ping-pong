4c7145e5176b8e6df4c9abb5c992e5107b0349ad
FafaF322 <lolte228@inbox.ru>
2026-05-08 09:33:03 +0300

    Update main.py
    добавил мяч и его отскоки

--- main.py
+++ main.py
@@ -27,6 +27,9 @@ class Player(GameSprite):
             self.rect.y += self.player_speed
 player_left = Player(7,0,170,'i.jpg',130,90)
 player_right = Player(7,620,170,'i.jpg',130,90)
+ball = GameSprite(3,350,250,'ball.jpg',65,65)
+speed_x = 3
+speed_y = 3
 
 fps = 60
 game = True
@@ -38,6 +41,13 @@ while game:
             game = False
     if finish != True:
         window.fill((255,255,255))
+        ball.reset()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if ball.rect.y >= 435 or ball.rect.y <= 0:
+            speed_y *= -1
+        if sprite.collide_rect(player_left,ball) or sprite.collide_rect(player_right,ball):
+            speed_x *= -1
         player_left.movement_left()
         player_left.reset()
         player_right.movement_right()
