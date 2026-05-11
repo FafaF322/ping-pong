30c2da2757f1c85a05b3f9cad05c027e3f331b63
FafaF322 <lolte228@inbox.ru>
2026-05-11 10:02:53 +0300

    Update main.py
    добавил условия победы и проигрыша

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pygame import *
+font.init()
 window = display.set_mode((700,500))
 display.set_caption('Пинг понг')
 window.fill((255,255,255))
@@ -25,11 +26,14 @@ class Player(GameSprite):
             self.rect.y -= self.player_speed
         if key_presed[K_DOWN] and self.rect.y < 420:
             self.rect.y += self.player_speed
+font1 = font.SysFont('Arial',50)
 player_left = Player(7,0,170,'i.jpg',130,90)
 player_right = Player(7,620,170,'i.jpg',130,90)
 ball = GameSprite(3,350,250,'ball.jpg',65,65)
 speed_x = 3
 speed_y = 3
+win = font1.render('PLAYER 1 WIN',True,(0,100,100))
+lose_win = font1.render('PLAYER 2 WIN',True,(100,0,0))
 
 fps = 60
 game = True
@@ -48,6 +52,12 @@ while game:
             speed_y *= -1
         if sprite.collide_rect(player_left,ball) or sprite.collide_rect(player_right,ball):
             speed_x *= -1
+        if ball.rect.x <= 0:
+            window.blit(lose_win,(150,200))
+            finish = True
+        if ball.rect.x >= 650:
+            window.blit(win,(150,200))
+            finish = True
         player_left.movement_left()
         player_left.reset()
         player_right.movement_right()
